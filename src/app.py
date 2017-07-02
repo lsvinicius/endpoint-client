d50b6bfe91b13adef7f9376dcca50b48ede8f476
vinicius <vinicius.lopes.silva89@gmail.com>
2017-07-02 00:39:00 -0300

    Adding custom messages for common errors

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 """Main application"""
 
 import os
-from flask import Flask
+from flask import Flask, render_template
 from flask_login import LoginManager
 from src.views.views import site_views
 from src.user import create_user
@@ -9,6 +9,15 @@ from src.user import create_user
 APP = Flask(__name__)
 
 APP.register_blueprint(site_views)
+
+@APP.errorhandler(404)
+@APP.errorhandler(401)
+@APP.errorhandler(403)
+@APP.errorhandler(500)
+def http_error_handler(error):
+    """Handler of the most common HTTP errors"""
+    return render_template('error.jinja', error=error), error.code
+
 
 # login_manager initialization
 LOGIN_MANAGER = LoginManager()
