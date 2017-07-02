3e76d2bb78ca218d10bdfb83b1afda4d9e8ec9cb
vinicius <vinicius.lopes.silva89@gmail.com>
2017-07-01 23:55:51 -0300

    Finished edit endpoint action

--- src/static/javascript/logged.js
+++ src/static/javascript/logged.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+    var fetch_data = true;
+    var interval_id = -1;
+    var edit_id=-1;
 
     function power(power_state, id) {
         var endpoint_action = "";
@@ -12,6 +15,26 @@ $(document).ready(function() {
             type: 'post',
             dataType: 'json',
             data: {"id":id},
+            success: function(data) {
+                console.log(data.message);
+            }
+        });
+    }   
+
+    function edit(id) {
+        var form = {};
+        form['id'] = id;
+        form['model'] = $("#row_"+id).children().eq(0).children().eq(0).val();
+        form['serialNumber'] = $("#row_"+id).children().eq(1).children().eq(0).val();
+        form['name'] = $("#row_"+id).children().eq(2).children().eq(0).val();
+        form['processor'] = $("#row_"+id).children().eq(3).children().eq(0).val();
+        form['memory'] = $("#row_"+id).children().eq(4).children().eq(0).val();
+        form['hd'] = $("#row_"+id).children().eq(5).children().eq(0).val();
+        $.ajax({
+            url: '/edit',
+            type: 'post',
+            dataType: 'json',
+            data: form,
             success: function(data) {
                 console.log(data.message);
             }
@@ -30,37 +53,8 @@ $(document).ready(function() {
         });
     }
 
-    function save_edit(id) {
-        var form_data = {
-            "model": $("#edit_model").val(),
-            "serialNumber": $("#edit_serialNumber").val(),
-            "name": $("#edit_name").val(),
-            "processor": $("#edit_processor").val(),
-            "memory": $("#edit_memory").val(),
-            "hd": $("#edit_hd").val(),
-            "id": id
-        };
-        $.ajax({
-            url: '/edit',
-            type: 'post',
-            dataType: 'json',
-            data: form_data,
-            success: function(data) {
-                var classType = data.success ? "alert alert-success fade in alert-dismissable" : 
-                                               "alert alert-danger fade in alert-dismissable";
-                $("#form_submit_result_edit").html(
-                    '<div class="'+classType+'">'+
-                        '<a href="#" class="close" data-dismiss="alert" aria-label="close">x</a>'+
-                        data.message +
-                    '</div>');
-            }
-        });
-    }
-
-    function edit(id) {
-         $("#edit_endpoint").html(
-             "<p>oi</p>"
-         );
+    function disable_toggle() {
+        $("#toggle_fetching_data").attr("disabled", true);
     }
 
     //handle endpoint actions
@@ -76,10 +70,60 @@ $(document).ready(function() {
             edit(id);
         } else if(action === 'power') {
             power(power_state, id);
+            disable_toggle();
         } else if(action === 'remove') {
             remove(id);
+            disable_toggle();
         }
     });
+
+    $(document.body).on('click', '#toggle_fetching_data', function() {
+        var text = $(this).text();
+        if(fetch_data) {
+            fetch_data = false;
+            $(this).text("Start fetching data");
+            $(this).attr('class', 'btn btn-success');
+            clearInterval(interval_id);
+            $('.item-row').each(function(index, item){
+                var current_username = $("#username").text();
+                var column_username = $(item).children().eq(7).children().eq(0).attr('value');
+                if(current_username === column_username) {
+                    $(this).children().eq(0).children().eq(0).attr('disabled', false);
+                    $(this).children().eq(1).children().eq(0).attr('disabled', false);
+                    $(this).children().eq(2).children().eq(0).attr('disabled', false);
+                    $(this).children().eq(3).children().eq(0).attr('disabled', false);
+                    $(this).children().eq(4).children().eq(0).attr('disabled', false);
+                    $(this).children().eq(5).children().eq(0).attr('disabled', false);
+                    $(this).children().eq(8).children().eq(0).attr('disabled', true);
+                    $(this).children().eq(9).children().eq(0).attr('disabled', false);
+                    $(this).children().eq(10).children().eq(0).attr('disabled', true);
+                }
+            });
+        } else {
+            fetch_data = true;
+            interval_id = setInterval(function() {
+                    draw_table();
+            }, 5000);
+            $(this).text("Stop fetching data");
+            $(this).attr('class', 'btn btn-warning');
+            $('.item-row').each(function(index, item){
+                var current_username = $("#username").text();
+                var column_username = $(item).children().eq(7).children().eq(0).attr('value');;
+                if(current_username === column_username) {
+                    $(this).children().eq(0).children().eq(0).attr('disabled', true);
+                    $(this).children().eq(1).children().eq(0).attr('disabled', true);
+                    $(this).children().eq(2).children().eq(0).attr('disabled', true);
+                    $(this).children().eq(3).children().eq(0).attr('disabled', true);
+                    $(this).children().eq(4).children().eq(0).attr('disabled', true);
+                    $(this).children().eq(5).children().eq(0).attr('disabled', true);
+                    $(this).children().eq(8).children().eq(0).attr('disabled', false);
+                    $(this).children().eq(9).children().eq(0).attr('disabled', true);
+                    $(this).children().eq(10).children().eq(0).attr('disabled', false);
+                }
+            });
+        }
+    });
+    
     //register new endpoint
     $("#create").click(function() {
         var form_data = {
@@ -112,15 +156,41 @@ $(document).ready(function() {
         var power = item.on ? "On" : "Off";
         var color = "#f2f4ed";
         var disabled = item.disabled ? "disabled" : "";
-        return '<tr bgcolor="'+color+'">\n' +
-                    '<td>'+item.model+'</td>\n' +
-                    '<td>'+item.serialNumber+'</td>\n' +
-                    '<td>'+item.name+'</td>\n' +
-                    '<td>'+item.processor+'</td>\n' +
-                    '<td>'+item.memory+'</td>\n' +
-                    '<td>'+item.hd+'</td>\n' +
-                    '<td>'+item.celsiusDegrees+'</td>\n' +
-                    '<td>'+item.user+'</td>\n' +
+
+        return '<tr bgcolor="'+color+'" class="item-row" id="row_'+item.id+'">\n' +
+                    '<td>'+
+                        '<input type="text" class="input-cell form-control" value="'+
+                            item.model+
+                    '" disabled/>'+
+                    '</td>\n' +
+                    '<td>'+
+                        '<input type="text" class="input-cell form-control" value="'+
+                            item.serialNumber+
+                    '" disabled/>'+
+                    '<td>'+
+                        '<input type="text" class="input-cell form-control" value="'+
+                        item.name+
+                    '" disabled/>' +
+                    '<td>'+
+                        '<input type="text" class="input-cell form-control" value="'+
+                        item.processor+
+                    '" disabled/>' +
+                    '<td>'+
+                        '<input type="text" class="input-cell form-control" value="'+
+                        item.memory+
+                    '" disabled/>' +
+                    '<td>'+
+                        '<input type="text" class="input-cell form-control" value="'+
+                        item.hd+
+                    '" disabled/>' +
+                    '<td>'+
+                        '<input type="text" class="input-cell form-control" value="'+
+                        item.celsiusDegrees+
+                    '" disabled/>'+
+                    '<td>'+
+                        '<input type="text" class="input-cell form-control" value="'+
+                        item.user+
+                    '" disabled/>' +
                     '<td>\n'+
                         '<button type="button" data-loading-text="<i class=\'fa fa-circle-o-notch fa-spin\'></i>"'+
                         'class="btn btn-md endpoint-action" id="power_'+item.id+'"'+
@@ -129,7 +199,7 @@ $(document).ready(function() {
                     '<td>'+
                         '<button type="button"'+
                         'class="btn btn-md endpoint-action" data-toggle="collapse" id="edit_'+item.id+'"'+
-                        disabled+'>Edit</button>' +
+                        'disabled>Edit</button>' +
                     '</td>\n' +
                     '<td>' +
                         '<button type="button" data-loading-text="<i class=\'fa fa-circle-o-notch fa-spin\'></i>"'+
@@ -158,8 +228,11 @@ $(document).ready(function() {
                                     'data-target="#addEndpoint">' +
                                 'Do you want to add one ?'+
                             '</button>'+
-                            '<div id="edit_endpoint">' +
-                            '</div>' +
+                            '<div class="btn-space">'+
+                            '<button type="button" id="toggle_fetching_data" class="btn btn-warning">'+
+                                'Stop fetching data'+
+                            '</button>'+
+                            '</div>'+
                         '</div>'+
                         '<div class="table-responsive">' +
                         '<table class = "table table-fixed">' +
@@ -189,7 +262,7 @@ $(document).ready(function() {
     draw_table();
 
     //list all endpoints without user constraints
-    setInterval(function() {
-        draw_table();
-    }, 5000);
+    interval_id = setInterval(function() {
+                    draw_table();
+            }, 5000);
 });
